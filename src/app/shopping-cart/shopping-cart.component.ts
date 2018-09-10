169b1ab045f57d20909f80523b7e96a9e367b46e
Vladimir Ruzic <vladimir@ruzic.info>
2018-09-10 03:16:18 +0200

    created details page and shopping cart manipulation

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,2 +1,14 @@
 import {Component, Injectable, OnInit } from '@angular/core';
 import { Router  } from '@angular/router';
+
+import {LocalStorageService} from '../local-storage.service';
+@Component({
+  selector: 'app-shirt-details',
+  templateUrl: './shopping-chart.component.html',
+  styleUrls: ['./shopping-chart.component.css'],
+  providers: [ LocalStorageService ]
+})
+@Injectable({ providedIn: 'root' })
+export class ShoppingCartComponent {
+
+}
